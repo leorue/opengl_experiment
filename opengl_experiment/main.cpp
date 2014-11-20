2f93b1b470892f09bba66036c850afdcc0607e27
Leo Schultz <schultz.leo@gmail.com>
2014-11-19 23:52:24 -0800

    Initial Commit

---
+++ opengl_experiment/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opengl_experiment
+//
+//  Created by Leo Schultz on 11/19/14.
+//  Copyright (c) 2014 Leo Schultz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
